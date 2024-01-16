f70ac5cd3c8bddbcc19d7e43ea0a5ee99a8a452f
carlos <carlos.ariza124@gmail.com>
2024-01-15 20:39:08 -0500

    corrigiendo ruta de fotos x2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       location: "Singapore",
       position: "Software Engineer",
       company: "Amazon",
-      url: "../dist/asian.jpg",
+      url: "../public/asian.jpg",
       description: "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
     },
     {
@@ -17,7 +17,7 @@ function App() {
       location: "Nigeria",
       position: "Software Engineer",
       company: "ChatDesk",
-      url: "../dist/african.jpg",
+      url: "../public/african.jpg",
       description: "freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
     },
     {
@@ -25,7 +25,7 @@ function App() {
       location: "Sweden",
       position: "Software Engineer",
       company: "Spotify",
-      url: "../dist/american.jpg",
+      url: "../public/american.jpg",
       description: "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
     },
   ];
